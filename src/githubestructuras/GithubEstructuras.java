47f457caf2c32ba423a545116d02bb430bcc2f7d
Usuario <Usuario@CEN309DOC>
2025-04-09 18:57:20 -0500

    este es un mensaje de commit

---
+++ src/githubestructuras/GithubEstructuras.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubestructuras;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GithubEstructuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola github!!!");
+        System.out.println("este es un cambio");
+    }
+    
+}
